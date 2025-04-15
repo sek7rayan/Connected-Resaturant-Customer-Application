3a25df038ca1bc7676df2011b03fc316db639d28
larabislem <islamlarabi0@gmail.com>
2025-04-15 14:55:54 +0100

    chore: Refactor API.js to handle errors, add API call for favorite plats, and remove unused dependencies

--- api.js
+++ api.js
@@ -3,7 +3,31 @@ import axios from "axios";
 const API_URL = "https://pfebackend-production.up.railway.app/api";
 
 
-const getFavoritePlats = async (id_client) => {
+const Api_plat_pref = {
+  addFavoritePlat : async (id_plat, id_client) => {
+    try {
+      const body = {
+        id_plat,
+        id_client,
+      };
+      const response = await axios.post('/platfavorie', body);
+      if (response.status === 201) {
+        return response.data; 
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error("Erreur serveur :", error.response.data.message);
+      } else if (error.request) {
+        console.error("Aucune réponse reçue du serveur.");
+      } else {
+        console.error("Erreur lors de la requête :", error.message);
+      }
+      throw error;
+    }
+  },
+
+
+  getFavoritePlats : async (id_client) => {
     try {
       const url = `${API_URL}/platfavorie/${id_client}`;
       const response = await axios.get(url);
@@ -24,6 +48,44 @@ const getFavoritePlats = async (id_client) => {
       }
       throw error;
     }
-  };
+  },
+
+
+  deleteFavoritePlat : async (id_client, id_plat) => {
+    try {
+      const url = `/platfavorie/${id_client}`;
+      await axios.delete(url, {
+        data: { id_plat },
+      });
   
-  export default getFavoritePlats;
+      console.log("Plat supprimé avec succès.");
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 404) {
+          console.error("Erreur : Plat non trouvé ou suppression impossible.");
+        } else {
+          console.error("Erreur serveur :", error.response.data.message);
+        }
+      } else if (error.request) {
+        console.error("Aucune réponse reçue du serveur.");
+      } else {
+        console.error("Erreur lors de la requête :", error.message);
+      }
+
+      throw error;
+    }
+  }
+  
+
+
+
+
+}
+
+
+
+
+export default Api_plat_pref;
+
+
+  
